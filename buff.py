0c18d7750ec1c395cb6e0511076f4d91b4d4a8f2
Minseo Lim <limms1217@naver.com>
2024-02-22 10:12:01 +0900

    algo buff commit

--- buff.py
+++ buff.py
@@ -1,22 +1,8 @@
 T = int(input())
-
-def go(idx,n, sum, num , op, statement):
-    if(idx == n):
-        sum += (num * op)
-        if(sum == 0):
-            print(statement)
-        return
-    # print('index',index)
-    # print('sum', sum)
-    # print('num', num)
-    # print('operation', operation)
-    go(idx+1, n, sum, (idx+1)+(10*num), op, statement + ' ' + str(idx+1))
-    go(idx+1, n, sum + (op * num), idx+1, 1, statement + '+' + str(idx+1))
-    go(idx+1, n, sum + (op * num), idx+1, -1, statement + '-' + str(idx+1))
-    
-
+book_page_array = [[] for _ in range(T)]
 for i in range(T):
-    n = int(input())
-    go(1, n, 0, 1, 1, '1')
+    K = int(input())
+    book_page_array[i] = list(map(int, input().split())) 
 
-
+for book_pages in book_page_array:
+    db = [[] * len(book_pages)]
