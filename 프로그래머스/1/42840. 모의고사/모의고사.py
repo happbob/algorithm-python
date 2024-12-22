60ea3dd46e4c87f4515a268e8520d230cdefda9a
Minseo Lim <limms1217@naver.com>
2024-12-22 11:20:12 +0900

    [level 1] Title: 모의고사, Time: 5.69 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,25 @@
+def solution(answers):
+    answer = [0,0,0]
+    player_1 = [1,2,3,4,5]
+    player_2 = [2,1,2,3,2,4,2,5]
+    player_3 = [3,3,1,1,2,2,4,4,5,5]
+    
+    for i in range(len(answers)):
+        if(answers[i] == player_1[(i % len(player_1)) ]): 
+            answer[0] += 1
+        if(answers[i] == player_2[(i % len(player_2))]):
+            answer[1] += 1
+        if(answers[i] == player_3[(i % len(player_3))]):
+            answer[2] += 1
+    max = answer[0]
+    sol = [1]
+    for i in range(1, len(answer)):
+        if(max == answer[i]):
+            sol.append(i+1)
+            max = answer[i]
+        elif(max < answer[i]):
+            sol.clear()
+            sol.append(i+1)
+            max = answer[i]
+
+    return sol
