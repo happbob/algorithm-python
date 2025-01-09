9b9e5f421f263368acfd556b4288257c411b3d5e
Minseo Lim <limms1217@naver.com>
2025-01-10 02:04:29 +0900

    [Bronze III] Title: 369, Time: 616 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -3,8 +3,6 @@ n = int(input())
 ans = 0
 three_array = ['3','6','9']
 for i in range(1,n+1):
-    for u in range(len(str(i))):
-        if(str(i)[u] in three_array):
-            ans+=1
+    ans += str(i).count("3") + str(i).count("6") + str(i).count("9")
 
 print(ans)
