8c1fcb1541475d54ea03bcb74fb13d242c591f93
Minseo Lim <limms1217@naver.com>
2025-01-11 15:01:55 +0900

    buff

--- buff.py
+++ buff.py
@@ -1,39 +1,14 @@
-import sys
+n = int(input()) 
 
-[n, m] = list(map(int, sys.stdin.readline().split()))
-board = [[0]*m for i in range(n)]
+def dfs(idx, num, sum, op, express):
+    global n
+    if(idx == n):
+        sum += num * op
+        if (sum == 0):
+            print(express)
+    
+    dfs(idx+1, num*10 + (idx + 1), sum, op, express + ' ' + (idx+1))
+    dfs(idx+1, idx + 1, sum + (num*op), 1, express + '+' + (idx+1))
+    dfs(idx+1, idx + 1, sum + (num*op), -1, express + '-' + (idx+1))
 
-for i in range(n):
-    buff = sys.stdin.readline()
-    for u in range(m):
-        board[i][u] = buff[u]
-
-cache_1 = [[0]*8 for i in range(8)]
-
-for i in range(8):
-    for u in range(8):
-        if(i % 2 == 0):
-            if(u%2==0):
-                cache_1[i][u] = 'B'
-            else:
-                 cache_1[i][u] = 'W'
-        else:
-            if(u%2==1):
-                cache_1[i][u] = 'B'
-            else:
-                 cache_1[i][u] = 'W'
-
-ans = 32
-
-for i in range(n-7):
-    for u in range(m-7):
-        buff=0
-        for x in range(8):
-            for y in range(8):
-                if (board[i+x][u+y] != cache_1[x][y]):
-                    buff += 1
-        
-        cal_min = min(buff, 64-buff)
-        ans = min(ans, cal_min)
-
-print(ans)
+dfs(1,1,0,1,"1")
