4ee8ab6c49b42d7500b47e82f836b7c11c1bf236
Minseo Lim <limms1217@naver.com>
2025-01-14 17:33:14 +0900

    feat: 이거야

--- buff.py
+++ buff.py
@@ -1,17 +1,22 @@
-from itertools import combinations, permutations
+T = int(input())
+
+def go(idx,n, sum, num , op, statement):
+    if(idx == n):
+        sum += (num * op)
+        if(sum == 0):
+            print(statement)
+        return
+    # print('index',index)
+    # print('sum', sum)
+    # print('num', num)
+    # print('operation', operation)
+    go(idx+1, n, sum, (idx+1)+(10*num), op, statement + ' ' + str(idx+1))
+    go(idx+1, n, sum + (op * num), idx+1, 1, statement + '+' + str(idx+1))
+    go(idx+1, n, sum + (op * num), idx+1, -1, statement + '-' + str(idx+1))
+    
+
+for i in range(T):
+    n = int(input())
+    go(1, n, 0, 1, 1, '1')
 
 
-n, m = map(int, input().split())
-array = [[] for _ in range(n)]
-for i in range(m):
-    buff1, buff2 = map(int, input().split())
-    array[min(buff1, buff2)].append(max(buff1, buff2))
-
-nums = [i for i in range(1, n+1)]
-combi = list(combinations(nums, 3))
-ans = len(combi)
-for i in range(1, n):
-    if ( len(array[i-1]) > 0):
-        ans -= (n-2-i+1) * len(array[i-1])
-
-print(ans)
