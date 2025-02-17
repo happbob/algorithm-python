a39d12a41a0f59e6478c29c4627ff3a8bb15f882
Minseo Lim <limms1217@naver.com>
2025-02-17 16:54:57 +0900

    [Gold IV] Title: 알파벳, Time: 968 ms, Memory: 15444 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -7,7 +7,7 @@ public class Main {
   static int ans = 0;
   static int[][] vector={{0,-1},{0,1},{1,0},{-1,0}};
   static HashSet<Character> hash;
-  static boolean[] fastCheck = new boolean[26];
+  static boolean[] visited = new boolean[26]; // 알파벳 방문 체크용 배열
   public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     hash = new HashSet<>();
@@ -17,10 +17,7 @@ public class Main {
     board = new char[R][C];
     
     for(int i=0;i<R;i++){
-      String buff = br.readLine();
-      for(int j=0;j<C;j++){
-        board[i][j] = buff.charAt(j);
-      }
+      board[i] = br.readLine().toCharArray(); // 빠른 입력 처리
     }
 
     letsgo(0,0,1);
@@ -29,25 +26,19 @@ public class Main {
 	}
 
   public static void letsgo(int x, int y, int count){
-    if (count + (R * C - count) <= ans) return; 
-
+    visited[board[x][y] - 'A'] = true; // 현재 알파벳 방문 표시
     ans = Math.max(ans, count);
-    char current = board[x][y];
-
-    hash.add(current);
     for(int i=0;i<4;i++){
       int newx = x+vector[i][0];
       int newy = y+vector[i][1];
-      fastCheck[current - 'A'] = true; // 빠른 contains 체크용
       // System.out.println(hash);
       if(0<=newx && newx<R && 0<=newy && newy<C){
-        char next = board[newx][newy];
-        if(!fastCheck[next-'A']) letsgo(newx,newy,count+1);
+        int nextCharIdx = board[newx][newy] - 'A';
+        if (!visited[nextCharIdx])  letsgo(newx,newy,count+1); // 방문한 적 없는 알파벳이면 탐색
         // System.out.println(newx + ", " +newy + " => "+count);
         
       }
     }
-    hash.remove(board[x][y]);
-    fastCheck[current-'A'] = false;
+    visited[board[x][y] - 'A'] = false;
   }
 }
