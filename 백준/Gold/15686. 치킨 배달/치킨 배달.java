0bcea878ed4293ad6520f110b16b4d5be5418908
Minseo Lim <limms1217@naver.com>
2025-03-07 11:39:32 +0900

    [Gold V] Title: 치킨 배달, Time: 212 ms, Memory: 18116 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,77 +1,71 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
-    static int M,N;
-    static int[][] board;
-    static int ans=Integer.MAX_VALUE;
-    static List<Chicken> chickenDeque;
-    static List<Node> houseDeque;
-    static boolean[] opened;
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        board = new int[N][N];
-        chickenDeque = new ArrayList();
-        houseDeque = new ArrayList();
+  static int M,N;
+  static List<Node> chickens, houses;
+  static int[][] board;
+  static boolean[] opened;
+  static int ans;
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i=0;i<N;i++){
-            st = new StringTokenizer(br.readLine());
-            for(int u=0;u<N;u++){
-                board[i][u] = Integer.parseInt(st.nextToken());
-                if(board[i][u]==1){
-                    houseDeque.add(new Node(i,u));
-                }else if(board[i][u]==2){
-                    chickenDeque.add(new Chicken(i,u,false));
-                }
-            }
-        }
-
-        opened = new boolean[chickenDeque.size()];
-        dfs(0,0);
-        System.out.println(ans);
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    chickens = new ArrayList<>();
+    houses = new ArrayList<>();
+    board = new int[N][N];
+    
+    for(int i=0;i<N;i++){
+      st = new StringTokenizer(br.readLine());
+      for(int j=0;j<N;j++){
+        board[i][j] = Integer.parseInt(st.nextToken());
+        if(board[i][j]==2) chickens.add(new Node(j,i));
+        if(board[i][j]==1) houses.add(new Node(j,i));
+      }
     }
 
-    public static void dfs(int start, int count){
-        if(count == M){
-            int result = 0;
-            for (int i=0;i<houseDeque.size();i++){
-                int temp = Integer.MAX_VALUE;
-                for (int j=0;j<chickenDeque.size();j++){
-                    if(opened[j]){
-                        int distance = Math.abs(houseDeque.get(i).x - chickenDeque.get(j).x) + Math.abs(houseDeque.get(i).y - chickenDeque.get(j).y);
-                        temp = Math.min(temp,distance);
-                    }
-                }
-                result += temp;
-            }
-            ans = Math.min(ans, result);
-            return;
-        }
+    ans = Integer.MAX_VALUE;
+    opened = new boolean[chickens.size()];
+    DFS(0,0);
+    System.out.println(ans);
 
-        for(int i=start;i<chickenDeque.size();i++){
-            opened[i]=true;
-            dfs(i+1,count+1);
-            opened[i]=false;
+  }
+
+  public static void DFS(int start, int cnt){
+    if(cnt==M){
+      int result = 0;
+      for(int i=0;i<houses.size();i++){
+        int buff = Integer.MAX_VALUE;
+        for(int j=0;j<chickens.size();j++){
+          if(opened[j]){
+            buff = Math.min(buff, Math.abs(houses.get(i).x - chickens.get(j).x) + Math.abs(houses.get(i).y - chickens.get(j).y));
+          }
         }
+        result += buff;
+      }
+
+      ans = Math.min(ans,result);
+      return;
     }
 
-    static class Node{
-        int x, y;
-        Node(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-    static class Chicken extends Node{
-        boolean opened;
+    // 백트래킹
+    for (int i = start; i < chickens.size(); i++) {
+      opened[i] = true;
+      DFS(i + 1, cnt + 1);
+      opened[i] = false;
+  }
 
-        Chicken(int x, int y, boolean opened) {
-            super(x, y);
-            this.opened = opened;
-        }
+
+  }
+
+  static class Node {
+    int x,y;
+    public Node(int x, int y){
+      this.x = x;
+      this.y = y;
     }
+  }
+  
 }
