9194ff397046bd2383c0bd3320cd039621502892
Minseo Lim <limms1217@naver.com>
2025-03-07 13:43:47 +0900

    [Gold V] Title: 치킨 배달, Time: 192 ms, Memory: 17448 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,71 +1,66 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-  static int M,N;
-  static List<Node> chickens, houses;
-  static int[][] board;
-  static boolean[] opened;
-  static int ans;
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
+    static int N,M;
+    static List<Node> chickens;
+    static List<Node> houses;
+    static Boolean[] visit;
+    static int min=Integer.MAX_VALUE;
 
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
-    chickens = new ArrayList<>();
-    houses = new ArrayList<>();
-    board = new int[N][N];
-    
-    for(int i=0;i<N;i++){
-      st = new StringTokenizer(br.readLine());
-      for(int j=0;j<N;j++){
-        board[i][j] = Integer.parseInt(st.nextToken());
-        if(board[i][j]==2) chickens.add(new Node(j,i));
-        if(board[i][j]==1) houses.add(new Node(j,i));
-      }
-    }
-
-    ans = Integer.MAX_VALUE;
-    opened = new boolean[chickens.size()];
-    DFS(0,0);
-    System.out.println(ans);
-
-  }
-
-  public static void DFS(int start, int cnt){
-    if(cnt==M){
-      int result = 0;
-      for(int i=0;i<houses.size();i++){
-        int buff = Integer.MAX_VALUE;
-        for(int j=0;j<chickens.size();j++){
-          if(opened[j]){
-            buff = Math.min(buff, Math.abs(houses.get(i).x - chickens.get(j).x) + Math.abs(houses.get(i).y - chickens.get(j).y));
-          }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        chickens = new ArrayList<Node>();
+        houses = new ArrayList<Node>();
+        for(int i=1;i<=N;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=1;j<=N;j++){
+                int buff = Integer.parseInt(st.nextToken());
+                if(buff == 2) chickens.add(new Node(j,i));
+                if(buff == 1) houses.add(new Node(j,i));
+            }
         }
-        result += buff;
-      }
+        visit=new Boolean[chickens.size()];
+        Arrays.fill(visit,false);
 
-      ans = Math.min(ans,result);
-      return;
+        dfs(0,0);
+
+        System.out.println(min);
     }
 
-    // 백트래킹
-    for (int i = start; i < chickens.size(); i++) {
-      opened[i] = true;
-      DFS(i + 1, cnt + 1);
-      opened[i] = false;
-  }
+    static void dfs(int start, int count) {
+        if(count==M){
+            int sum=0;
+            for(int i=0;i<houses.size();i++){
+                int buff = Integer.MAX_VALUE;
+                for(int j=0;j<chickens.size();j++){
+                    if(visit[j]){
+                        int dist = Math.abs(houses.get(i).x-chickens.get(j).x) + Math.abs(houses.get(i).y - chickens.get(j).y);
+                        buff = Math.min(buff, dist);
+                    }
+                }
+                sum+=buff;
+            }
+            min = Math.min(sum,min);
+            return;
+        }
 
-
-  }
-
-  static class Node {
-    int x,y;
-    public Node(int x, int y){
-      this.x = x;
-      this.y = y;
+        // 백트래킹
+        for(int i = start;i<chickens.size();i++){
+            visit[i] = true;
+            dfs(i+1, count+1);
+            visit[i] = false;
+        }
     }
-  }
-  
+
+    static class Node{
+        int x,y;
+        Node(int x, int y){
+            this.x=x;
+            this.y=y;
+        }
+    }
 }
