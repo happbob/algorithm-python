ad1100071b62664ab68cdec397890c8984d12e92
Minseo Lim <limms1217@naver.com>
2025-03-14 09:56:26 +0900

    [Gold III] Title: 택배, Time: 712 ms, Memory: 33764 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -2,26 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
+    static int N,M;
     static Integer[][] board;
-    static PriorityQueue<Vertex> queue;
-    static Integer[][] first;
     static Integer[][] cost;
+    static Integer[][] first;
+    static PriorityQueue<Vertex> queue;
+    
     public static void main(String[] args) throws IOException{
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         board = new Integer[N+1][N+1];
         first = new Integer[N+1][N+1];
         cost = new Integer[N+1][N+1];
-
         queue = new PriorityQueue<Vertex>();
-
-        for(int i=1;i<=M;i++){
+        for(int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine());
             int v1 = Integer.parseInt(st.nextToken());
             int v2 = Integer.parseInt(st.nextToken());
@@ -38,8 +34,10 @@ public class Main {
         }
 
         for(int i=1;i<=N;i++){
-            daik(i);
+            // 시작하는 집하장을 변경해주면서 각 집하장 간의 최소 비용 및 제일 먼저 거쳐야 하는 집하장 업데이트
+            go(i);
         }
+
         StringBuffer sb = new StringBuffer();
         for(int i=1;i<=N;i++){
             for(int j=1;j<=N;j++){
@@ -48,29 +46,28 @@ public class Main {
             }
             sb.append("\n");
         }
+
         System.out.println(sb);
     }
 
-    static void daik(int start){
+    static void go(int start){
         Integer[] firstToGo = new Integer[N+1];
         for(int i=1;i<=N;i++){
             if(i==start) firstToGo[i] = 0;
-            else firstToGo[i] = i;
+            else firstToGo[i]=i;
         }
         queue.offer(new Vertex(start,0));
-
         while(!queue.isEmpty()){
-            Vertex v = queue.poll();
+            Vertex vertex = queue.poll();
             for(int i=1;i<=N;i++){
-                if(board[v.v][i]!=null){
-                    if(cost[start][i] > cost[start][v.v] + board[v.v][i]){
-                        cost[start][i] = cost[start][v.v] + board[v.v][i];
-                        
-                        queue.offer(new Vertex(i,board[v.v][i]));
+                if(board[vertex.v][i]==null) continue;
+                if(i==vertex.v) continue;
 
-                        if(v.v==start) continue;
-                        firstToGo[i] = firstToGo[v.v];
-                    }
+                if(cost[start][i] > cost[start][vertex.v] + board[vertex.v][i]){
+                    cost[start][i] = cost[start][vertex.v] + board[vertex.v][i];
+                    queue.offer(new Vertex(i,board[vertex.v][i]));
+                    if(vertex.v==start) continue;
+                    firstToGo[i] = firstToGo[vertex.v];
                 }
             }
         }
