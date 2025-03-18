a62716a85b92a64bb2021920d1845c75877043f4
Minseo Lim <limms1217@naver.com>
2025-03-18 23:18:48 +0900

    [Gold IV] Title: 오큰수, Time: 820 ms, Memory: 152584 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -2,30 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
-    Integer n = Integer.parseInt(br.readLine());
-    Integer[] arr = new Integer[n+1];
-    StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    for(int i = 1;i<=n;i++){
-      arr[i] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        int[] number = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] ret = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = n - 1; i >= 0; i--) {
+            ret[i] = -1;
+
+            while (!stack.isEmpty() && number[i] >= stack.peekFirst()) {
+                stack.pollFirst();
+            }
+            if (!stack.isEmpty()) {
+                ret[i] = stack.peekFirst();
+            }
+            stack.addFirst(number[i]);
+        }
+
+        for (int r : ret) {
+            bw.write(String.valueOf(r));
+            bw.write(' ');
+        }
+        bw.flush();
+        br.close();
+        bw.close();
     }
-    Stack<Integer> stack = new Stack<Integer>();
-    for(int i=1; i<=n;i++){
-      while(!stack.isEmpty() && arr[stack.peek()] < arr[i]){
-        arr[stack.pop()] = arr[i];
-      }
-      stack.push(i);
-    }
-    while(!stack.isEmpty()){
-      arr[stack.pop()] = -1;
-    }
-		for(int i = 1; i <= n; i++) {
-			sb.append(arr[i]).append(' ');
-		}
-		
-		System.out.println(sb);
-  }
+
+
 }
