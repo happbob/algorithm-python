054a68518244d258f2209a65a597c3d5075d4f08
Minseo Lim <limms1217@naver.com>
2025-04-18 16:54:22 +0900

    [Gold III] Title: 나무 재테크, Time: 836 ms, Memory: 302148 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -1,170 +1,124 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
 
-// 백준 온라인 저지 16235 나무재테크 문제 풀이
 public class Main {
-	static int N;					// 전체 땅의 행과 열
-	static int M;					// 처음에 주어진 나무의 개수
-	static int K;					// 년수
-	static int[][] foods;			// 땅마다 추가되는 양분
-	static int[][] lands;			// 전체 땅
-	static ArrayList<Tree> trees = new ArrayList<>();		// 나무들		
-	static Deque<Integer> deadTrees = new LinkedList<>();	// 죽은 나무들
+    static int N,M,K;
+    static int[][] board;
+    static int[][] nutrient;
+    static List<Tree> trees;
+    static int[][] vector = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        board = new int[N+1][N+1];
+        nutrient = new int[N+1][N+1];
+        List<Tree> trees = new ArrayList<>();
+        for(int i=1;i<=N;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=1;j<=N;j++){
+                nutrient[i][j] = Integer.parseInt(st.nextToken());
+                board[i][j] = 5;
+            }
+        }
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int age = Integer.parseInt(st.nextToken());
+            Tree tree = new Tree(x,y,age);
+            trees.add(tree);
+            
+        }
+        Collections.sort(trees);
+        
+        while(K-->0){
+            // System.out.println(K + "년 차례임");
+            List<Tree> liveTrees = new ArrayList<>();
+            List<Tree> deadTrees = new ArrayList<>();
+            // 봄
+            for(int i=0;i<trees.size();i++){
+                
+                Tree buff = trees.get(i); 
 
-	public static void main(String[] args) throws IOException {
-		// 입력정보 받고 초기화
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String[] N_M_K = reader.readLine().split(" ");
-		N = Integer.parseInt(N_M_K[0]);
-		M = Integer.parseInt(N_M_K[1]);
-		K = Integer.parseInt(N_M_K[2]);
-		foods = new int[N][N];
-		lands = new int[N][N];
+                // System.out.println(buff.toString());
+                // System.out.println("헤헤 양분 먹어야징" + buff.x + ", " + buff.y + " : " + buff.age);
+                if(buff.age <= board[buff.x][buff.y]){
+                    // 땅에 양분이 더 높다면?
+                    board[buff.x][buff.y] -= buff.age;
+                    buff.age++;
+                    liveTrees.add(buff);
+                }else{
+                    deadTrees.add(buff);
+                }
+            }
 
-		// 매년마다 추가 될 양분 정보 받기, 초기 땅의 양분 초기화
-		for (int i = 0; i < N; i++) {
-			String[] food = reader.readLine().split(" ");
-			for (int j = 0; j < N; j++) {
-				foods[i][j] = Integer.parseInt(food[j]);
-				lands[i][j] = 5;
-			}
-		}
+            // System.out.println(Arrays.toString(liveTrees.toArray()));
+            // System.out.println(Arrays.toString(deadTrees.toArray()));
+            // 여름
+            for(int i=0;i<deadTrees.size();i++){
+                Tree buff = deadTrees.get(i); 
+                board[buff.x][buff.y] += (int) buff.age/2;
+            }
+            // System.out.println("가을이 오기전... 살아남은 녀석들 size : " + liveTrees.size());
 
-		// 초기에 주어진 나무들 
-		for (int i = 0; i < M; i++) {
-			String[] tree = reader.readLine().split(" ");
-			trees.add(new Tree(tree));
-		}
+            // 가을
+            trees = new ArrayList<>();
+            for(int i=0;i<liveTrees.size();i++){
+                Tree buff = liveTrees.get(i); 
+                if(buff.age % 5 == 0){
+                    for(int j=0;j<8;j++){
+                        int newX = buff.x + vector[j][0];
+                        int newY = buff.y + vector[j][1];
+                        if(newX > 0 && newX <= N && newY>0 && newY<=N){
+                            trees.add(new Tree(newX,newY,1));
+                        }
+                    }
+                }
+            }
 
-		// 처음에 주어진 나무들을 어린나이 순으로 정렬
-		Collections.sort(trees, (t1, t2) -> t1.age - t2.age);
-		
-		// 주어진 년수가 0일 때까지 반복
-		while (K != 0) {
-			spring();		// 봄
-			summer();		// 여름
-			fall();			// 가을
-			winter();		// 겨울
-			K--;			// 년수 감소
-		}
+            // System.out.println("겨울이 왔어... size : " + trees.size());
+            // 겨울
+            for(int i=1;i<=N;i++){
+                for(int j=1;j<=N;j++){
+                    board[i][j] += nutrient[i][j];
+                }
+            }
+            trees.addAll(liveTrees);
+            Collections.sort(trees);
+            if(trees.size()==0){
+                System.out.println(0);
+                return;
+            }
 
-		// 땅에 있는 전체 나무 개수 출력
-		System.out.println(trees.size());
-	}
+            // System.out.println("1년이 갔어... size : " + trees.size());
+            // System.out.println(Arrays.toString(trees.toArray()));
+        }
 
-	/**
-	 * 봄
-	 */
-	public static void spring() {
-		// 현재 나무들 전체 탐색
-		for (int i = 0; i < trees.size(); i++) {
-			Tree tree = trees.get(i);
-			// 땅의 양분이 나무의 나이보다 적으면
-			if (lands[tree.row][tree.col] < tree.age) {
-				// 나무는 죽음
-				deadTrees.add(i);
-				continue;
-			}
-			// 아니라면 땅의 양분 먹고 나무 나이 1증가
-			lands[tree.row][tree.col] -= tree.age;
-			tree.age++;
-		}
-	}
+        System.out.println(trees.size());
+        
+    }
 
-	/**
-	 * 여름
-	 */
-	public static void summer() {
-		// 죽은 나무들 하나씩 탐색
-		while (!deadTrees.isEmpty()) {
-			// 죽은 나무의 인덱스 꺼내기
-			int deadTreeIndex = deadTrees.pollLast();
-			// 인덱스로 죽은 나무 찾기
-			Tree deadTree = trees.get(deadTreeIndex);
-			// 죽은 나무의 나이 / 2 만큼 양분 땅에 추가
-			int food = deadTree.age / 2;
-			lands[deadTree.row][deadTree.col] += food;
-			// 나무는 죽음
-			deadTree.dead = true;
-		}
-	}
+    static class Tree implements Comparable<Tree>{
+        int x,y,age;
+        public Tree(int x, int y, int age){
+            this.x = x;
+            this.y = y;
+            this.age = age;
+        }
 
-	/**
-	 * 가을
-	 */
-	public static void fall() {
-		// 8군데 좌표계산에 사용할 배열
-		int[] moveRow = {-1, -1, -1, 0, 0, 1, 1, 1};
-		int[] moveCol = {-1, 0, 1, -1, 1, -1, 0, 1};
-		// 새로 생성될 나무들 저장할 리스트
-		ArrayList<Tree> newTrees = new ArrayList<>();
-		// 현재 나무들 전체 탐색
-		for (int p = 0; p < trees.size(); p++) {
-			Tree tree = trees.get(p);
-			// 죽은 나무는 건너뛰기
-			if (tree.dead) {
-				continue;
-			}
-			// 살아있는 나무의 나이가 5의 배수라면
-			if (tree.age % 5 == 0) {
-				// 전체 땅을 벗어나지 않을 때 번식
-				for (int i = 0; i < 8; i++) {
-					int nr = tree.row + moveRow[i];
-					int nc = tree.col + moveCol[i];
-					if (nr < 0 || nr >= N || nc < 0 || nc >= N) {
-						continue;
-					}
-					// 나이가 1인 나무 생성
-					newTrees.add(new Tree(nr, nc, 1));
-				}
-			}
-		}
-		// 새로 생성된 나무들이 저장된 리스트에 기존의 살아있는 나무들 추가
-		for (Tree tree : trees) {
-			if (!tree.dead) {
-				newTrees.add(tree);
-			}
-		}
-		// 기존 나무 + 새로 생성된 나무 
-		trees = newTrees;
-	}
+        @Override
+        public int compareTo(Tree t){
+            return this.age - t.age;
+        }
 
-	/**
-	 * 겨울
-	 */
-	public static void winter() {
-		// 땅마다 새로 양분 추가
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				lands[i][j] += foods[i][j];
-			}
-		}
-	}
+        @Override
+        public String toString(){
+            return x + ", " + y + " = " + age;
+        }
+    }
 
-	// 나무
-	static class Tree {
-		int row;		// 나무가 있는 행
-		int col;		// 나무가 있는 열
-		int age;		// 나무의 나이
-		boolean dead;	// 나무 생존 여부
-
-		public Tree(String[] tree) {
-			this.row = Integer.parseInt(tree[0]) - 1;
-			this.col = Integer.parseInt(tree[1]) - 1;
-			this.age = Integer.parseInt(tree[2]);
-		}
-
-		public Tree(int row, int col, int age) {
-			this.row = row;
-			this.col = col;
-			this.age = age;
-		}
-	}
 }
 
