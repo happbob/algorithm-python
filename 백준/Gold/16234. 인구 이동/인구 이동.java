0c570bd1a57ec3c96354324e0156685d78fee8c4
Minseo Lim <limms1217@naver.com>
2025-05-15 00:02:11 +0900

    [Gold IV] Title: 인구 이동, Time: 576 ms, Memory: 297608 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -9,52 +9,55 @@ import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer; 
+
 public class Main {
-    static BufferedReader br; 
     static int N,L,R;
     static int[][] board;
     static boolean[][] visited;
+    static int[][] vector = {{0,1},{0,-1},{1,0},{-1,0}};
     static ArrayList<int[]> list;
-
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1}; // 상하좌우순서
     public static void main(String[] args) throws Exception {
-        br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         L = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
         board = new int[N][N];
+        visited = new boolean[N][N];
         for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
             for(int ii=0;ii<N;ii++){
-                board[i][ii] = Integer.parseInt(st.nextToken());
+                int buff = Integer.parseInt(st.nextToken());
+                board[i][ii] = buff;
             }
         }
-        int result = 0;
-        while (true) {
+
+        int ans = 0;
+        while(true){ 
             boolean isMove = false;
             visited = new boolean[N][N];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (!visited[i][j]) {
-                        int sum = dfs(i, j); // 방문하지 않은 곳 dfs 탐색
-                        if (list.size() > 1) {
-                            changePopulation(sum); // 국경이 열린 노드끼리 인구 이동
+            for(int i=0;i<N;i++){
+                for(int ii=0;ii<N;ii++){
+                    if(!visited[i][ii]){
+                        int sum = DFS(i,ii);
+                        if(list.size()>1){
+                            changePopulation(sum);
                             isMove = true;
                         }
                     }
                 }
             }
-            if (!isMove) {
+
+            if(!isMove){
                 break;
             }
-            result++;
-        }
+            ans++;
 
-        System.out.println(result);
+        }
+        System.out.println(ans);
     }
-    
+
+
     public static void changePopulation(int sum) {
         int avg = sum / list.size();
         for (int i = 0; i < list.size(); i++) {
@@ -64,31 +67,31 @@ public class Main {
         }
     }
 
-    public static int dfs(int x, int y) {
+
+    static public int DFS(int x,int y){
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{x, y});
-
         list = new ArrayList<>();
         list.add(new int[]{x, y});
 
         visited[x][y] = true;
         int sum = board[x][y];
-
         while (!queue.isEmpty()) {
             int tmp[] = queue.poll();
-            for (int i = 0; i < 4; i++) { // 4방탐색
-                int nx = tmp[0] + dx[i];
-                int ny = tmp[1] + dy[i];
-                if (nx >= 0 && nx < N && ny >= 0 && ny < N) { // graph 범위 내
-                    // 방문한적 없고, L과 R 사이일 때
-                    if (!visited[nx][ny] && L <= Math.abs(board[tmp[0]][tmp[1]] - board[nx][ny]) && Math.abs(board[tmp[0]][tmp[1]] - board[nx][ny]) <= R) {
-                        visited[nx][ny] = true; // 방문한곳으로 변경
-                        queue.offer(new int[]{nx, ny}); // 새로 큐에 담아줌
-                        list.add(new int[]{nx, ny}); // 연결된 나라끼리는 따로 좌표를 담아줌
-                        sum += board[nx][ny];
-                    }
+            for(int i=0;i<4;i++){
+                int newx = tmp[0] + vector[i][0];
+                int newy =  tmp[1] + vector[i][1];
+
+                if(newx >=0 && newx<N &&newy >=0 && newy<N &&!visited[newx][newy] &&  Math.abs(board[ tmp[0]][ tmp[1]]-board[newx][newy])<=R&& Math.abs(board[ tmp[0]][ tmp[1]]-board[newx][newy])>=L){
+                    visited[newx][newy] = true;
+                    queue.offer(new int[]{newx, newy}); // 새로 큐에 담아줌
+                    list.add(new int[]{newx, newy}); // 연결된 나라끼리는 따로 좌표를 담아줌
+                    sum += board[newx][newy];
                 }
+            
+                
             }
+            
         }
         return sum;
     }
